d7574f2cf2467e9d615a26f37a2e5e9ac963d25e
Lojitao <Lojitao@outlook.com>
2024-10-04 19:23:45 +0800

    [更新]環境檔、docker-compose.yml

--- config/config.js
+++ config/config.js
@@ -1,25 +1,32 @@
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+require('dotenv').config({ path: envFile });
+console.log('process.env.DB_CONTAINER_PORT',process.env.DB_CONTAINER_PORT);
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "clwy1234",
-    database: process.env.DB_DATABASE || "clwy_api_development",
-    host: process.env.DB_HOST || "clwy-api",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_CONTAINER_PORT,
     dialect: "mysql",
     timezone: "+08:00",
     LogQueryParameters: true
   },
   test: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_DATABASE || "clwy_api_development",
-    host: process.env.DB_HOST || "clwy-api",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_CONTAINER_PORT,
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_DATABASE || "clwy_api_development",
-    host: process.env.DB_HOST || "clwy-api",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_CONTAINER_PORT,
     dialect: "mysql"
   }
 };
+
